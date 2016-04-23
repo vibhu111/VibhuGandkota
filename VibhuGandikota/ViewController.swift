2173bbe880e18e9719cc6581793460ac29c3ac11
Vibhu Gandikota <Vibhu@vibhus-mbp.fios-router.home>
2016-04-23 16:25:17 -0400

    Initial Commit

---
+++ VibhuGandikota/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VibhuGandikota
+//
+//  Created by Vibhu Gandikota on 4/23/16.
+//  Copyright © 2016 Vibhu Gandikota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
